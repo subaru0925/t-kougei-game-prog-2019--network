f1f2f9cc6d20543b589262ecdde96fe245c2ce46
subaru0925 <38637442+subaru0925@users.noreply.github.com>
2019-08-05 13:47:02 +0900

    Update ClientSample.cpp

--- src/ClientSample/ClientSample.cpp
+++ src/ClientSample/ClientSample.cpp
@@ -18,14 +18,15 @@ int main(void)
 	// サーバのアドレス情報を設定
 	struct sockaddr_in dest;
 	memset(&dest, 0, sizeof(dest));
-	****
-	InetPton(****);
+	dest.sin_port = htons(PORT_NO);
+	dest.sin_family = AF_INET;
+	InetPton(dest.sin_family, destination, &dest.sin_addr.S_un.S_addr);
 
 	// ソケットの生成
 	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
 
 	//サーバへの接続
-	if (****) {
+	if (connect(s, (sockaddr *)&dest, sizeof(dest))) {
 		printf("%lsに接続できませんでした\n", destination);
 		return -1;
 	}
@@ -37,7 +38,7 @@ int main(void)
 
 		char send_data[1024];
 		scanf_s("%s", send_data, 1024);
-		if (****) {
+		if (send(s, send_data, strlen(send_data) + 1, 0) == SOCKET_ERROR) {
 			printf("送信エラー\n");
 		}
 
@@ -46,7 +47,7 @@ int main(void)
 
 		// サーバからデータを受信
 		char buffer[1024];
-		if (****) {
+		if (recv(s, buffer, sizeof(buffer), 0) == SOCKET_ERROR) {
 			printf("受信エラー\n");
 		}
 		printf("応答がきました: %s\n", buffer);
@@ -54,7 +55,8 @@ int main(void)
 
 	// ソケット通信の終了
 	printf("接続終了\n");
-	****
+	closesocket(s);
+	WSACleanup();
 
 	return 0;
 }
